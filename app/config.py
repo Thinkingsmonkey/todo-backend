88b4f40fb6f5a27ba39804933961f3800640b1ac
Neal <neal99185@gmail.com>
2023-09-22 16:15:17 +0800

    change to deploy config

--- app/config.py
+++ app/config.py
@@ -6,11 +6,11 @@ class Config():
   ERROR_404_HELP = False
 
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')  # 部署環境使用
-  SQLALCHEMY_DATABASE_URI = "mysql://root:12345678@localhost/todo"  # 開發環境使用
+  # SQLALCHEMY_DATABASE_URI = "mysql://root:12345678@localhost/todo"  # 開發環境使用
   SQLALCHEMY_TRACK_MODIFICATIONS = False
 
   JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') # 部署環境使用
-  JWT_SECRET_KEY = "this secret key" # 開發環境使用
+  # JWT_SECRET_KEY = "this secret key" # 開發環境使用
   JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=60)
   JWT_TOKEN_LOCATION = ["headers", "cookies"]  
   JWT_CSRF_IN_COOKIES = False
